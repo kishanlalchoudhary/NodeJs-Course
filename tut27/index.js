7ca80ca66d8a9f131464fb553b1bec8d993fc9c1
Kishanlal Choudhary <kisanchoudhary000@gmail.com>
2023-06-09 23:25:12 +0530

    mongoDB basics

---
+++ tut27/index.js
@@ -0,0 +1,12 @@
+// Install MongoDB
+
+// Topic -
+// Download DB
+// Install MongoDB
+// Set Environment for Mongo
+// Mongodb compass tool
+// Interview Question
+
+// Q1 - How to setup mongoDB setup
+// Q2 - Name any GUI tool - MongoDB Compass
+// Q3 - Difference between MongoDB and MYSQL
---
+++ tut28/index.js
@@ -0,0 +1,17 @@
+// MongoDB Basic
+
+// Topics - 
+// What is MongoDB
+// MongoDB vs SQL
+// MongoDB basic commands
+// Interview Question
+
+// Commands in Mongosh
+// show dbs - it is used to list all dbs
+// use <db_name> - to create a new db and also can be used to switch between dbs in mongosh
+// new db is only visible when there is atleast one collection
+// db.createCollection("collection_name") - it is used to create a collection with given collection name
+// show collections - to view all collections
+// db.<collection_name>.drop() - it is used to delete a collection from a particular database
+// db - it is used to check in which database your mongosh is operated
+// db.dropDatabase() - it is used to delete a database in which your mongosh is operating
